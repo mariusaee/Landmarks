e970b0ebc0b01cc7c6d8dd36becc627ffb580fb1
mariusaee <mariusae@me.com>
2021-05-19 16:28:32 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Marius Malyshev on 19.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
