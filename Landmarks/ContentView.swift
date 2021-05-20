b1032411369e0803133cf5b50709e4ac797e2e36
mariusaee <mariusae@me.com>
2021-05-20 13:49:10 +0300

    Text() Circle image Map Compose

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,8 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            MapView()
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Tutrle Rock")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                
+                HStack {
+                    Text("Joshua Three National Park")
+                    Spacer()
+                    Text("Calofirnnia")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here")
+            }
             .padding()
+            Spacer()
+        }
     }
 }
 
